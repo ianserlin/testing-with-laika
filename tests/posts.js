cd6d7721297e5ec3ac070d3674460d732d7f8b85
ianserlin <ian@ianserlin.com>
2014-03-18 23:54:02 -0700

    testing laika failure case

--- tests/posts.js
+++ tests/posts.js
@@ -24,6 +24,25 @@ suite('Posts', function () {
         emit('post', post);
       }
     }).once('post', function(post) {
+      assert.fail('this should fail', 'it must fail!', 'donkeys fail');
+      done();
+    });
+
+    client.eval(function() {
+      Posts.insert({title: 'hello title'});
+    });
+  });
+  
+    test('Failed test', function(done, server, client) {
+    server.eval(function() {
+      Posts.find().observe({
+        added: addedNewPost
+      });
+
+      function addedNewPost(post) {
+        emit('post', post);
+      }
+    }).once('post', function(post) {
       assert.equal(post.title, 'hello title');
       done();
     });
@@ -31,5 +50,5 @@ suite('Posts', function () {
     client.eval(function() {
       Posts.insert({title: 'hello title'});
     });
-  });;
+  });
 })
